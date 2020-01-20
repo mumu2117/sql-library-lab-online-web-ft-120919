2a4c4ff9b09a727d0af82a99c9ca2ccaf6ebe09b
Jennifer Miller <jmillerarcher@me.com>
2020-01-20 23:44:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,13 +32,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title
-  FROM characters
-  INNER JOIN series
-  ON characters.series_id = series.id
-  GROUP BY Series.title, species
-  HAVING characters.species = 'human'
-  ORDER BY COUNT(species) DESC
+ "SELECT series.title FROM series INNER JOIN authors ON authors.id = series.author_id INNER JOIN characters ON authors.id = characters.author_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(characters.species) DESC
   LIMIT 1;"
 end
 
